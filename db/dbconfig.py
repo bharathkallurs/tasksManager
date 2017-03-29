b4a65811b00ed5e2e9f8e0ca8626a7bc129ca8f2
bharath kallur <bharathkallurs@gmail.com>
2017-03-30 02:23:20 +0530

    adding heroku default mongodb store to MONGO_URI

--- db/dbconfig.py
+++ db/dbconfig.py
@@ -2,8 +2,9 @@ import os
 from pymongo import MongoClient
 
 #Database setting
+
 MONGO_URI = os.environ.get('MONGOHQ_URL') if os.environ.get('MONGOHQ_URL') is not None \
-    else 'mongodb://localhost:27017/heroku_tr2f0kzz'
+    else 'mongodb://heroku_tr2f0kzz:6vgb9kukuk3tg1berdjegh3vn4@ds033175.mlab.com:33175/heroku_tr2f0kzz'
 
 client = MongoClient(MONGO_URI)
 DB = client.get_default_database().name if client.get_default_database() is not None else 'task_database'
